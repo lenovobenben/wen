fe0786b21f01ca68eb8c62dfb3f7f46eaf8b1a2d
lihd <lenovobenben@gmail.com>
2020-12-13 21:50:58 +0800

    初始化项目

---
+++ src/main/java/dp/Solution.java
@@ -0,0 +1,12 @@
+package dp;
+
+/**
+ * 斐波那契数列
+ * （从0开始，第0项为0，第1项是1）
+ */
+public class Solution {
+
+    public int Fibonacci(int n) {
+        return 0;
+    }
+}
---
+++ src/main/java/sort/Solution.java
@@ -0,0 +1,13 @@
+package sort;
+
+/**
+ * 输入一个正整数数组，将它们连接起来排成一个数，输出能排出的所有数字中最大的一个
+ * 比如 75,9,7,76,54,79,112,776 ，应该输出 9,79,7,776,76,75,54,112
+ */
+public class Solution {
+
+    public String solve(int[] nums) {
+        return null;
+    }
+
+}
