835580603f280bfdb8e40c26a823a891b71b4fae
lihd <lenovobenben@gmail.com>
2020-12-19 19:29:49 +0800

    ...

--- src/main/java/dp/Solution.java
+++ src/main/java/dp/Solution.java
@@ -1,5 +1,7 @@
 package dp;
 
+import java.math.BigInteger;
+
 /**
  * 斐波那契数列
  * （从0开始，第0项为0，第1项是1）
@@ -10,15 +12,28 @@ public class Solution {
     /**
      * 动态规划
      */
-    public int Fibonacci(int n) {
-        return 0;
+    public BigInteger Fibonacci(int n) {
+        if (n==0) {
+            return new BigInteger("0");
+        } else if (n==1) {
+            return new BigInteger("1");
+        } else {
+            BigInteger[] dp = new BigInteger[n + 1];// dp[i] 对应着 i的斐波那契值
+            dp[0] = new BigInteger("0");;// 初始值
+            dp[1] = new BigInteger("1");;// 初始值
+            for (int i = 2; i < n + 1; i++) {
+                dp[i] = dp[i-1].add(dp[i-2]);
+            }
+            return dp[n];
+        }
     }
 
 
     /**
-     * 递归
+     * 递归 存在大量重复计算
      */
     public int Fibonacci2(int n) {
+
         if (n == 0) {
             return 0;
         } else if (n == 1) {
@@ -29,6 +44,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().Fibonacci2(11));
+        System.out.println(new Solution().Fibonacci(10000));
     }
 }
