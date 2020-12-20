7fed8b6632dbc74db106e48cc4a82015cf6e9d61
lihd <lenovobenben@gmail.com>
2020-12-20 13:40:31 +0800

    ...

---
+++ src/main/java/binarySearch/Solution.java
@@ -0,0 +1,39 @@
+package binarySearch;
+
+/**
+ * 从0,1,2,...,n这n+1个数中选择n个数，组成有序数组，找出这n个数中缺失的那个数，要求O(n)尽可能小。
+ * [0,1,2,3,4,5,7]
+ * 缺失 6
+ */
+public class Solution {
+
+    public int solve(int[] a) {
+        return 0;
+    }
+
+    /**
+     * 经典 二分查找
+     */
+    public int binarySearch(int[] arr, int hkey) {
+        int result = -1;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;//防止溢位
+            if (arr[mid] > hkey)
+                end = mid - 1;
+            else if (arr[mid] < hkey)
+                start = mid + 1;
+            else {
+                result = mid;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 6, 8, 9};
+        System.out.println(new Solution().binarySearch(arr, 7));
+    }
+}
