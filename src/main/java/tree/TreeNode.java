6af16b6bb8023a27e285f378f827ee06e0dc38d7
lihd <lenovobenben@gmail.com>
2020-12-27 09:53:02 +0800

    ...

---
+++ src/main/java/tree/TreeNode.java
@@ -0,0 +1,11 @@
+package tree;
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
