2b8fea1ae35e6ef46713a34af6c6e63dd85a01e4
lihd <lenovobenben@gmail.com>
2020-12-27 10:06:46 +0800

    ...

--- src/main/java/tree/Solution.java
+++ src/main/java/tree/Solution.java
@@ -6,6 +6,23 @@ package tree;
  */
 public class Solution {
     public TreeNode Convert(TreeNode pRootOfTree) {
+        TreeNode root = Solution.buildTree();
+
         return null;
+    }
+
+
+    public static TreeNode buildTree() {
+        TreeNode root = new TreeNode(7);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(9);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(6);
+        root.right.left = new TreeNode(8);
+        root.right.right = new TreeNode(10);
+        root.left.left.left = new TreeNode(2);
+        root.left.left.right = new TreeNode(4);
+        root.left.left.left.left = new TreeNode(1);
+        return root;
     }
 }
