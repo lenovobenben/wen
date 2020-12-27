8c663801d3743c0803ab8492b7c6a2c055f94c16
lihd <lenovobenben@gmail.com>
2020-12-27 17:53:28 +0800

    ...

---
+++ src/main/java/tree/Solution2.java
@@ -0,0 +1,11 @@
+package tree;
+
+/**
+ * 平衡的定义如下，已知对于树中的任意一个结点，若其两颗子树的高度差不超过1，则我们称该树平衡。
+ * 现给定指向树根结点的指针TreeNode* root，请编写函数返回一个bool，表示该二叉树是否平衡。
+ */
+public class Solution2 {
+    public boolean isBalance(TreeNode root) {
+        return false;
+    }
+}
