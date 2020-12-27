b9917a0c5a5f3de4400e12c3cf9b2afc43c82ea2
lihd <lenovobenben@gmail.com>
2020-12-27 18:43:39 +0800

    ...

--- src/main/java/tree/Solution2.java
+++ src/main/java/tree/Solution2.java
@@ -5,7 +5,22 @@ package tree;
  * 现给定指向树根结点的指针TreeNode* root，请编写函数返回一个bool，表示该二叉树是否平衡。
  */
 public class Solution2 {
+
     public boolean isBalance(TreeNode root) {
-        return false;
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(height(root.left) - height(root.right)) <=1 && isBalance(root.left) && isBalance(root.right);
     }
+
+    /**
+     * 求树的最大高度（深度）
+     */
+    public int height (TreeNode node) {
+        if (node==null) {
+            return 0;
+        }
+        return Math.max(height(node.left),height(node.right)) + 1;
+    }
+
 }
