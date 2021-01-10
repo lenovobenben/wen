486b94efad2f88f07648e6dfa6c0e33123cd0868
lihd <lenovobenben@gmail.com>
2021-01-10 11:08:35 +0800

    ...

---
+++ src/main/java/twoPointers/Solution.java
@@ -0,0 +1,12 @@
+package twoPointers;
+
+import linkedList.ListNode;
+
+/**
+ * 判断给定的链表中是否有环
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        return false;
+    }
+}
---
+++ src/main/java/twoPointers/Solution2.java
@@ -0,0 +1,10 @@
+package twoPointers;
+
+/**
+ * 给定一个数组 arr ，返回arr的 最长无重复子串 的长度(无重复指的是所有数字都不相同)。
+ */
+public class Solution2 {
+    public int maxLength(int[] arr) {
+        return 0;
+    }
+}
