ac4d07b48a58bd621bfa4ca8e8db3b81eb8b71c8
lihd <lenovobenben@gmail.com>
2021-01-16 08:20:20 +0800

    ...

---
+++ src/main/java/stackQueue/Solution.java
@@ -0,0 +1,18 @@
+package stackQueue;
+
+import java.util.Stack;
+
+/**
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ */
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+    }
+
+    public int pop() {
+        return 0;
+    }
+}
