4a7622c71b46083a1e354b06b2192c4c0d954ffb
lihd <lenovobenben@gmail.com>
2021-01-16 19:48:49 +0800

    ...

---
+++ src/main/java/stackQueue/Solution3.java
@@ -0,0 +1,14 @@
+package stackQueue;
+
+import tree.TreeNode;
+
+import java.util.ArrayList;
+
+/**
+ * 给定一个二叉树，返回该二叉树层序遍历的结果，（从左到右，一层一层地遍历）
+ */
+public class Solution3 {
+    public ArrayList<ArrayList<Integer>> levelOrder (TreeNode root) {
+        return null;
+    }
+}
