df555dcea02eccac0421a7ab1e45de42aeab531d
lihd <lenovobenben@gmail.com>
2021-01-16 15:17:51 +0800

    ...

--- src/main/java/stackQueue/Solution2.java
+++ src/main/java/stackQueue/Solution2.java
@@ -1,5 +1,7 @@
 package stackQueue;
 
+import java.util.Stack;
+
 /**
  * 给出一个仅包含字符'(',')','{','}','['和']',的字符串，判断给出的字符串是否是合法的括号序列
  * 括号必须以正确的顺序关闭，"()"和"()[]{}"都是合法的括号序列，但"(]"和"([)]"不合法。
@@ -9,7 +11,28 @@ package stackQueue;
 public class Solution2 {
 
     public boolean isValid(String s) {
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (c=='(') {
+                stack.push(')');
+            } else if (c=='[') {
+                stack.push(']');
+            } else if (c=='{') {
+                stack.push('}');
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (stack.pop()!=c) {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
