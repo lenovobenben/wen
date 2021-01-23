594f8e7c94323a4b26d6628664eadd322122e849
lihd <lenovobenben@gmail.com>
2021-01-23 10:01:54 +0800

    ...

--- src/main/java/hash/Solution.java
+++ src/main/java/hash/Solution.java
@@ -1,5 +1,7 @@
 package hash;
 
+import java.util.HashMap;
+
 /**
  * 给出一个整数数组，请在数组中找出两个加起来等于目标值的数，
  * 你给出的函数twoSum 需要返回这两个数字的下标（index1，index2），需要满足 index1 小于index2.。注意：下标是从1开始的
@@ -11,6 +13,14 @@ package hash;
 public class Solution {
 
     public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.get(numbers[i])!=null) {
+                return new int[]{map.get(numbers[i])+1,i+1};
+            } else {
+                map.put(target-numbers[i],i);
+            }
+        }
         return null;
     }
 }
