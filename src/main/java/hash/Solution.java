d26da19937caf63e724935ab4f0f6879726aec03
lihd <lenovobenben@gmail.com>
2021-01-23 09:04:56 +0800

    ...

---
+++ src/main/java/hash/Solution.java
@@ -0,0 +1,16 @@
+package hash;
+
+/**
+ * 给出一个整数数组，请在数组中找出两个加起来等于目标值的数，
+ * 你给出的函数twoSum 需要返回这两个数字的下标（index1，index2），需要满足 index1 小于index2.。注意：下标是从1开始的
+ * 假设给出的数组中只存在唯一解
+ * 例如：
+ * 给出的数组为 {20, 70, 110, 150},目标值为90
+ * 输出 index1=1, index2=2
+ */
+public class Solution {
+
+    public int[] twoSum(int[] numbers, int target) {
+        return null;
+    }
+}
