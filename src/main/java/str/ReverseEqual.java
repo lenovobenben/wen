8a1e38ed99bdd94fceae58878e894ad70370e5ed
lihd <lenovobenben@gmail.com>
2021-01-24 11:22:27 +0800

    ...

---
+++ src/main/java/str/ReverseEqual.java
@@ -0,0 +1,11 @@
+package str;
+
+/**
+ * 给定2个字符串s1和s2，请判断s2是否为s1旋转而成，返回bool值。
+ * 字符串中字符为英文字母和空格，区分大小写，字符串长度小于等于1000。
+ */
+public class ReverseEqual {
+    public boolean checkReverseEqual(String s1, String s2) {
+        return false;
+    }
+}
