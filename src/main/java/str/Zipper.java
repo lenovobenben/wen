f4d0026534f06b5fc309a74560b384c6e4fbe766
lihd <lenovobenben@gmail.com>
2021-01-24 08:51:04 +0800

    ...

---
+++ src/main/java/str/Zipper.java
@@ -0,0 +1,11 @@
+package str;
+
+/**
+ * 现给定一个string iniString字符串(长度小于等于10000)，请按连续重复字母压缩的方式将该字符串压缩，返回结果为string
+ * 比如，字符串“aabbcccccaaa”经压缩会变成“a2b2c5a3”，若压缩后的字符串没有变短，则返回原先的字符串。注意保证串内字符均由大小写英文字母组成。
+ */
+public class Zipper {
+    public String zip(String str) {
+        return null;
+    }
+}
