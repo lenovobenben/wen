38000365c08559af37683598ff29318f48b20107
lihd <lenovobenben@gmail.com>
2021-03-21 12:40:14 +0800

    200. 岛屿数量

--- src/main/java/matrix/Solution3.java
+++ src/main/java/matrix/Solution3.java
@@ -2,7 +2,7 @@ package matrix;
 
 /**
  * 200. 岛屿数量
- *
+ * <p>
  * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
  * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
  * 此外，你可以假设该网格的四条边均被水包围。
@@ -26,21 +26,14 @@ class Solution3 {
      * 以 x,y 坐标展开岛屿，将值设置为2
      */
     public void dfs(char[][] grid, int x, int y) {
-
-        if (grid[x][y] == '1') {// 只有陆地才处理
-            grid[x][y] = '2';
-            if (grid[0].length > y + 1) {
-                dfs(grid, x, y + 1);// 右
-            }
-            if (grid.length > x + 1) {
-                dfs(grid, x + 1, y);// 下
-            }
-            if (y - 1 >= 0) {
-                dfs(grid, x, y - 1);// 左
-            }
-            if (x - 1 >= 0) {
-                dfs(grid, x - 1, y);// 上
-            }
+        if (x < 0 || y < 0 || y >= grid[0].length || x >= grid.length || grid[x][y] != '1') {
+            return;
         }
+        grid[x][y] = '2';
+        dfs(grid, x, y + 1);// 右
+        dfs(grid, x + 1, y);// 下
+        dfs(grid, x, y - 1);// 左
+        dfs(grid, x - 1, y);// 上
     }
+
 }
