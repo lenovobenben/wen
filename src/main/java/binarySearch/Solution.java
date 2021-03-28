660a57d6eede0f13496482b3d1273ad745fb6fb2
lihd <lenovobenben@gmail.com>
2021-03-28 11:25:04 +0800

    牛客->力扣

--- src/main/java/binarySearch/Solution.java
+++ src/main/java/binarySearch/Solution.java
@@ -8,27 +8,27 @@ package binarySearch;
  * 缺失 6
  */
 public class Solution {
-    public int solve(int[] a) {
+    public int missingNumber(int[] a) {
         // 特殊处理 最后一位
-        if (a[a.length-1]==a.length-1) {
+        if (a[a.length - 1] == a.length - 1) {
             return a.length;
         }
 
         // 特殊处理 第一位
-        if (a[0]!=0) {
+        if (a[0] != 0) {
             return 0;
         }
 
         int start = 0;
         int end = a.length - 1;
         while (true) {
-            if (end - start == 1) {
-                return  end;
+            if (end - start == 1) {// 相差1时，大的下标值即为结果
+                return end;
             }
             int temp = (start + end) / 2;
             if (a[temp] > temp) {
                 end = temp;
-            } else {
+            } else {// 此时 a[temp] == temp 。 不存在小于的情况
                 start = temp;
             }
         }
