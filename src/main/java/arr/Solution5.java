bd1fe74ef3698f606a7ed3ecd372db5e557af083
lihd <lenovobenben@gmail.com>
2021-05-05 10:17:47 +0800

    优化代码

--- src/main/java/arr/Solution5.java
+++ src/main/java/arr/Solution5.java
@@ -16,4 +16,14 @@ public class Solution5 {
         }
         return dp[prices.length - 1];
     }
+
+    public int maxProfit_(int[] prices) {
+        int maxProfit = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return maxProfit;
+    }
 }
