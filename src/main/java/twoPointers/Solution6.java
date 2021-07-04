a1783260faf5f157825fa96b9ba14b066e32d63b
lihd <lenovobenben@gmail.com>
2021-07-04 18:23:36 +0800

    。。。

--- src/main/java/twoPointers/Solution6.java
+++ src/main/java/twoPointers/Solution6.java
@@ -8,18 +8,20 @@ public class Solution6 {
         int start = 0;
         int end = nums.length - 1;
         while (start < end) {
-            if ((nums[start] & 1) == 1) {// 奇数
+            if ((nums[start] & 1) == 1) {// start是奇数
                 start++;
-            } else {// 偶数，等待互换
-                if ((nums[end] & 1) == 1) {// 奇数
-                    int temp = nums[start];
-                    nums[start] = nums[end];
-                    nums[end] = temp;
-                    start++;
-                    end--;
-                } else {// 也是偶数
-                    end--;
-                }
+                continue;
+            }
+            if ((nums[end] & 1) == 0) {// end是偶数
+                end--;
+                continue;
+            }
+            if ((nums[start] & 1) == 0 && (nums[end] & 1) == 1) {// start偶数 同时 end奇数。互换
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
             }
         }
         return nums;
