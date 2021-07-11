e8606266915c1b0a6289afb46625dd06844bb3d4
lihd <lenovobenben@gmail.com>
2021-07-11 11:26:52 +0800

    剑指 Offer 61. 扑克牌中的顺子

---
+++ src/main/java/math/Solution.java
@@ -0,0 +1,23 @@
+package math;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 剑指 Offer 61. 扑克牌中的顺子
+ */
+public class Solution {
+    // 抄大佬的答案
+    public boolean isStraight(int[] nums) {
+        Set<Integer> repeat = new HashSet<>();
+        int max = 0, min = 14;
+        for (int num : nums) {
+            if (num == 0) continue; // 跳过大小王
+            max = Math.max(max, num); // 最大牌
+            min = Math.min(min, num); // 最小牌
+            if (repeat.contains(num)) return false; // 若有重复，提前返回 false
+            repeat.add(num); // 添加此牌至 Set
+        }
+        return max - min < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
+    }
+}
