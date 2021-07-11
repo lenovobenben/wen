1fd94f1d1d4013c845490e252ad02902af8afa97
lihd <lenovobenben@gmail.com>
2021-07-11 12:31:34 +0800

    剑指 Offer 61. 扑克牌中的顺子

--- src/main/java/math/Solution.java
+++ src/main/java/math/Solution.java
@@ -13,9 +13,9 @@ public class Solution {
         int max = 0, min = 14;
         for (int num : nums) {
             if (num == 0) continue; // 跳过大小王
+            if (repeat.contains(num)) return false; // 若有重复，提前返回 false
             max = Math.max(max, num); // 最大牌
             min = Math.min(min, num); // 最小牌
-            if (repeat.contains(num)) return false; // 若有重复，提前返回 false
             repeat.add(num); // 添加此牌至 Set
         }
         return max - min < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
